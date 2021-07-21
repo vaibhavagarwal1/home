b5ef005ab32a03ca25e7ce17fbace1fd47455b0f
vaibhav <vaibhav@kali.kali>
2021-07-22 04:58:46 +0530

    final

---
+++ forms/contact.php
@@ -0,0 +1,28 @@
+<?php
+ 
+  $receiving_email_address = 'avaibhav547@gmail.com';
+
+  
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  $contact->smtp = array(
+    'host' => 'notvalid99999@gmail.com',
+    'username' => 'vaibhav',
+    'password' => 'totallyinvalid',
+    'port' => '587'
+  );
+  
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
